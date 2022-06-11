68b36e6d0b8191659b8e405c8882baf20c222cb2
F-Errami <errami2000@gmail.com>
2022-06-12 00:32:13 +0100

    create the ui design

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile
 from services.video_services import save_video
-from va2speech.va2speech import video_2_speech
+from utils.va2speech import video_2_speech
 
 app = FastAPI()
 
